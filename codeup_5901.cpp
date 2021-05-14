8aa03214fa1734307a3de843f75d0a3c84dfa591
Cain-Iriving <54472321+Cain-Iriving@users.noreply.github.com>
2021-05-14 13:16:50 +0800

    Update codeup_5901.cpp

--- codeup_5901.cpp
+++ codeup_5901.cpp
@@ -4,6 +4,13 @@ const int maxn = 256;
 
 //判断字符串str是否是回文串
 bool judge(char str[]){
+    int len = strlen(str); //回文串
+    for(int i = 0;i<len/2;++i){//i枚举字符串的前一半
+        if(str[i]!=str[len-1-i]){
+          return false; 
+        }
+    }
+    return true;
 }
 
 int main(){
